2de1089e046203a66ab916cc4a90a6bf67151e0c
Vyacheslav Kan <vkan745@gmail.com>
2024-09-16 10:24:31 +0500

    1. Visibility. Общий ресурс вне критической секции [#1102 #513189]

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.function.Predicate;
 
 public class ParseFile {
 
@@ -10,27 +11,16 @@ public class ParseFile {
         this.file = file;
     }
 
-    public synchronized String getContent() throws IOException {
-        String output = "";
+    public synchronized String getContent(Predicate<Integer> condition) throws IOException {
+        StringBuilder output = new StringBuilder();
         try (InputStream input = new FileInputStream(file)) {
             int data;
             while ((data = input.read()) > 0) {
-                output += (char) data;
-            }
-        }
-        return output;
-    }
-
-    public synchronized String getContentWithoutUnicode() throws IOException {
-        String output = "";
-        try (InputStream input = new FileInputStream(file)) {
-            int data;
-            while ((data = input.read()) > 0) {
-                if (data < 0x80) {
-                    output += (char) data;
+                if (condition.test(data)) {
+                    output.append((char) data);
                 }
             }
         }
-        return output;
+        return output.toString();
     }
 }
