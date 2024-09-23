9a32dfc4b6b067858ce190f74d8ff44362b0b04c
Vyacheslav Kan <vkan745@gmail.com>
2024-09-23 12:22:18 +0500

    1. Реализовать ThreadPool [#1099] (корректировка 23.09)

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -7,16 +7,22 @@ import java.util.List;
 
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(2);
+    private volatile boolean running = true;
 
     public ThreadPool() {
         int size = Runtime.getRuntime().availableProcessors();
         for (int i = 0; i < size; i++) {
             Thread thread = new Thread(() -> {
-                try {
-                    tasks.poll();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
+                while (running || !tasks.isEmpty()) {
+                    try {
+                        Runnable task = tasks.poll();
+                        if (task != null) {
+                            task.run();
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
                 }
             });
             threads.add(thread);
@@ -25,13 +31,31 @@ public class ThreadPool {
     }
 
     public void work(Runnable job) throws InterruptedException {
-        tasks.offer(job);
+        if (running) {
+            tasks.offer(job);
+        }
     }
 
     public void shutdown() throws InterruptedException {
+        running = false;
         for (Thread thread : threads) {
-            thread.join();
             thread.interrupt();
+            thread.join();
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool pool = new ThreadPool();
+        Runnable task0 = () -> System.out.println("Выполнение задачи №0 - " + Thread.currentThread().getName());
+        Runnable task1 = () -> System.out.println("Выполнение задачи №1 - " + Thread.currentThread().getName());
+        Runnable task2 = () -> System.out.println("Выполнение задачи №2 - " + Thread.currentThread().getName());
+        Runnable task3 = () -> System.out.println("Выполнение задачи №3 - " + Thread.currentThread().getName());
+        Runnable task4 = () -> System.out.println("Выполнение задачи №4 - " + Thread.currentThread().getName());
+        pool.work(task0);
+        pool.work(task1);
+        pool.work(task2);
+        pool.work(task3);
+        pool.work(task4);
+        pool.shutdown();
     }
 }
