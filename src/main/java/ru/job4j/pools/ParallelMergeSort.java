333b666f587f51fb545a1ee5a80075839c51224b
Vyacheslav Kan <vkan745@gmail.com>
2024-09-25 17:48:54 +0500

    3. ForkJoinPool [#315067] (удалены однострочные комментарии)

--- src/main/java/ru/job4j/pools/ParallelMergeSort.java
+++ src/main/java/ru/job4j/pools/ParallelMergeSort.java
@@ -18,17 +18,13 @@ public class ParallelMergeSort extends RecursiveTask<int[]> {
     @Override
     protected int[] compute() {
         if (from == to) {
-            return new int[] {array[from]};
+            return new int[]{array[from]};
         }
         int middle = (from + to) / 2;
-        // создаем задачи для сортировки частей
         ParallelMergeSort leftSort = new ParallelMergeSort(array, from, middle);
         ParallelMergeSort rightSort = new ParallelMergeSort(array, middle + 1, to);
-        // производим деление.
-        // оно будет происходить, пока в частях не останется по одному элементу
         leftSort.fork();
         rightSort.fork();
-        // объединяем полученные результаты
         int[] left = leftSort.join();
         int[] right = rightSort.join();
         return MergeSort.merge(left, right);
