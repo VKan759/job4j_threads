f212b7c247ecf09bafe7042a5095f0bf08c569e7
Vyacheslav Kan <vkan745@gmail.com>
2024-09-25 10:53:00 +0500

    2. ExecutorService рассылка почты. [#63097 #514182] (корректировка 25.09)

--- src/main/java/ru/job4j/email/EmailNotification.java
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -1,16 +1,15 @@
 package ru.job4j.email;
 
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
     private final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-    public List<String> emailTo(User user) {
+    public void emailTo(User user) {
         String subject = String.format("Notification {%s} to email {%s}", user.getUserName(), user.getEmail());
         String body = String.format("Add a new event to %s", user.getUserName());
-        return List.of(subject, body);
+        executor.submit(() -> send(subject, body, user.getEmail()));
     }
 
     public void send(String subject, String body, String email) {
@@ -18,18 +17,19 @@ public class EmailNotification {
 
     public void close() {
         executor.shutdown();
+        while (!executor.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
         User user = new User("Vyacheslav", "vkan745@gmail.com");
         EmailNotification notification = new EmailNotification();
-        notification.executor.submit(new Runnable() {
-            @Override
-            public void run() {
-                List<String> email = notification.emailTo(user);
-                notification.send(email.get(0), email.get(1), user.getEmail());
-            }
-        });
+        notification.emailTo(user);
         notification.close();
     }
 }
