7ae3b2347e46125c25f459df8b590982ce93a46e
Vyacheslav Kan <vkan745@gmail.com>
2024-09-26 13:53:13 +0500

    3. ForkJoinPool [#315067] (приведено к общему типу)

--- src/main/java/ru/job4j/pools/ForkJoinPoolEx.java
+++ src/main/java/ru/job4j/pools/ForkJoinPoolEx.java
@@ -1,20 +1,25 @@
 package ru.job4j.pools;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class ForkJoinPoolEx extends RecursiveTask<Integer> {
+public class ForkJoinPoolEx<T> extends RecursiveTask<Integer> {
     int from;
     int to;
-    Object[] array;
-    Object target;
+    T[] array;
+    T target;
 
-    public ForkJoinPoolEx(Object[] array, int from, int to, Object target) {
+    public ForkJoinPoolEx(T[] array, int from, int to, T target) {
         this.from = from;
         this.to = to;
         this.array = array;
         this.target = target;
     }
 
+    public static <T> int findElementIndex(T[] array, T element) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return forkJoinPool.invoke(new ForkJoinPoolEx<>(array, 0, array.length - 1, element));
+    }
 
     @Override
     protected Integer compute() {
@@ -22,19 +27,19 @@ public class ForkJoinPoolEx extends RecursiveTask<Integer> {
             return find(target);
         }
         int mid = (from + to) / 2;
-        ForkJoinPoolEx left = new ForkJoinPoolEx(array, from, mid, target);
-        ForkJoinPoolEx right = new ForkJoinPoolEx(array, mid + 1, to, target);
+        ForkJoinPoolEx<T> left = new ForkJoinPoolEx<>(array, from, mid, target);
+        ForkJoinPoolEx<T> right = new ForkJoinPoolEx<>(array, mid + 1, to, target);
         left.fork();
         right.fork();
         int leftResult = left.join();
         int rightResult = right.join();
-        return leftResult != -1 ? leftResult : rightResult;
+        return Math.max(leftResult, rightResult);
     }
 
-    public Integer find(Object target) {
+    public Integer find(T target) {
         int result = -1;
-        for (int i = 0; i < to; i++) {
-            if (array[i] == target) {
+        for (int i = from; i <= to; i++) {
+            if (array[i].equals(target)) {
                 result = i;
                 break;
             }
--- src/test/java/ru/job4j/pools/ForkJoinPoolExTest.java
+++ src/test/java/ru/job4j/pools/ForkJoinPoolExTest.java
@@ -2,42 +2,33 @@ package ru.job4j.pools;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ForkJoinPool;
-
 import static org.assertj.core.api.Assertions.*;
 
 class ForkJoinPoolExTest {
     @Test
     public void lineFindElement() {
-
+        int target = 2;
         Integer[] array = {1, 2, 3, 4, 5, 6};
-        ForkJoinPoolEx forkJoinPoolEx = new ForkJoinPoolEx(array, 0, array.length - 1, 2);
-        ForkJoinPool pool = new ForkJoinPool();
-        assertThat(pool.invoke(forkJoinPoolEx)).isEqualTo(1);
+        assertThat(ForkJoinPoolEx.findElementIndex(array, target)).isEqualTo(1);
     }
 
     @Test
     public void findElement() {
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
         Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        ForkJoinPoolEx forkJoinPoolEx = new ForkJoinPoolEx(array, 0, array.length - 1, 2);
-        assertThat(forkJoinPool.invoke(forkJoinPoolEx)).isEqualTo(1);
+        int target = 2;
+        assertThat(ForkJoinPoolEx.findElementIndex(array, target)).isEqualTo(1);
     }
 
     @Test
     public void findUser() {
-        ForkJoinPool pool = new ForkJoinPool();
-        User user = new User("Vyacheslav", 1);
-        Object[] elements = {1, 2, 3, 4, 5, user, 6, 7, 8, 9, 10, 11};
-        ForkJoinPoolEx forkJoinPoolEx = new ForkJoinPoolEx(elements, 0, elements.length - 1, user);
-        assertThat(pool.invoke(forkJoinPoolEx)).isEqualTo(5);
+        User target = new User("Vyacheslav", 1);
+        Object[] elements = {6, 7, 8, 9, 10, target, 11};
+        assertThat(ForkJoinPoolEx.findElementIndex(elements, target)).isEqualTo(5);
     }
 
     @Test
     public void elementNotFound() {
-        ForkJoinPool pool = new ForkJoinPool();
         Object[] array = {1, 21, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        ForkJoinPoolEx example = new ForkJoinPoolEx(array, 0, array.length - 1, 14);
-        assertThat(pool.invoke(example)).isEqualTo(-1);
+        assertThat(ForkJoinPoolEx.findElementIndex(array, 14)).isEqualTo(-1);
     }
 }
